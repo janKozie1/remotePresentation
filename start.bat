64f1952c04a031a28d6c868f126bd3ff42d5762c
janKozie1 <jankkoziel@gmail.com>
2019-08-07 12:15:03 +0200

    Documentation

--- start.bat
+++ start.bat
@@ -2,15 +2,38 @@
 set "str1=%~dp0"
 set "str2=node_modules"
 set "str3=%str1%%str2%"
-
+set "str4=Media"
+set "str5=%str1%%str4%"
+echo Starting... 
 IF EXIST %str3% (  
- echo Starting... 	
- node pres_service.js --add
- pause
+ IF NOT EXIST %str5% (
+  mkdir Media
+  echo defaultSlideDuration = 10 > Media/_config.txt
+  node pres_service.js --add
+  pause
+ )else (
+  echo defaultSlideDuration = 10 > Media/_config.txt
+  node pres_service.js --add
+  pause
+ )
 ) ELSE (
  npm install
- node pres_service.js --add
- pause
+ IF NOT EXIST %str5% (
+  mkdir Media
+  echo defaultSlideDuration = 10 > Media/_config.txt
+  node pres_service.js --add
+  pause
+ )else (
+  echo defaultSlideDuration = 10 > Media/_config.txt
+  node pres_service.js --add
+  pause
+ )
 )
+
+
+
+
+
+
 
 
